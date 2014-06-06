d2d768a3c4e0d6184636bbaf041fba807fdbdf02
Lahiru Dharmasena <lahiru82@gmail.com>
2014-06-06 13:10:52 +0530

    answer engine

--- web.js
+++ web.js
@@ -332,12 +332,19 @@ app.get('/getQuestion', function(req, res){
     
     var html = "<div>" +
           "<fieldset>" +
-            "<legend>Alexander died in ?</legend>" +
+            "<legend>Question 1) Alexander died in ?</legend>" +
             "<div><input type='radio' id='aq_radios' value='0' name='question1'/><span>Persia</span></div>" +
             "<div><input type='radio' id='aq_radios' value='1' name='question1'/><span>India</span></div>" +
             "<div><input type='radio' id='aq_radios' value='2' name='question1'/><span>Babylon</span></div>" +
             "<div><input type='radio' id='aq_radios' value='3' name='question1'/><span>Macedon</span></div>" +
           "</fieldset>" +
+          "<fieldset>" +
+          "<legend>Question 2) Alexander was tutored by ?</legend>" +
+          "<div><input type='radio' id='aq_radios' value='0' name='question1'/><span>David Hume</span></div>" +
+          "<div><input type='radio' id='aq_radios' value='1' name='question1'/><span>Plato</span></div>" +
+          "<div><input type='radio' id='aq_radios' value='2' name='question1'/><span>Aristotle</span></div>" +
+          "<div><input type='radio' id='aq_radios' value='3' name='question1'/><span>John Rawls</span></div>" +
+        "</fieldset>" +
           "<div><input type='button' value='submit' onclick='checkAnswer()'></div>" +
   "</div>";
     
